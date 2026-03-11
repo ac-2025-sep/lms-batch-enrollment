96284f44267bf135dab76c224444a2b57bd7b60b
ac-2025-sep <abhay.chauhan@staqo.com>
2026-03-11 19:34:28 +0530

    Include userops_reports URLs in urlpatterns

--- userops/urls.py
+++ userops/urls.py
@@ -1,8 +1,8 @@
 from django.urls import include, path
-
 from . import views
 
 urlpatterns = [
     path("userops/", views.dashboard, name="userops-dashboard"),
+    path("userops/", include("userops_reports.urls")),
     path("api/userops/v1/", include("userops.urls_api")),
 ]
