5b320b81479272eef0bcd703f650ed3c7bac4222
ac-2025-sep <abhay.chauhan@staqo.com>
2026-03-12 23:13:40 +0530

    Uncomment userops_reports URL include

--- userops/urls.py
+++ userops/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     path("userops/", views.dashboard, name="userops-dashboard"),
-    #path("userops/", include("userops_reports.urls")),
+    path("userops/", include("userops_reports.urls")),
     path("api/userops/v1/", include("userops.urls_api")),
 ]
